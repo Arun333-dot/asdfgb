e91f8c9cdba11ce4c18e3ca1f73e7dd0aed0134f
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-24 05:54:22 +0000

    dsdsdsads

---
+++ sql_simple_External_project/models/model1.sql
@@ -0,0 +1,50 @@
+WITH million_records AS (
+
+  SELECT * 
+  
+  FROM {{ source('hive_metastore.arun123', 'million_records') }}
+
+),
+
+Reformat_1 AS (
+
+  SELECT * 
+  
+  FROM million_records AS in0
+
+),
+
+ordered_reformat_1 AS (
+
+  {#Organizes chronic patient records in ascending order by ID.#}
+  SELECT * 
+  
+  FROM Reformat_1 AS in0
+  
+  ORDER BY id ASC NULLS FIRST
+
+),
+
+Limit_1 AS (
+
+  SELECT * 
+  
+  FROM ordered_reformat_1 AS in0
+  
+  LIMIT 10
+
+),
+
+Filter_1 AS (
+
+  SELECT * 
+  
+  FROM Limit_1 AS in0
+  
+  WHERE true
+
+)
+
+SELECT *
+
+FROM Filter_1
