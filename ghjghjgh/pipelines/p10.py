e98a851fcab8a9cfefd6b607a672b0e4e138c2c2
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-26 13:43:47 +0000

    dfgdfgd

--- ghjghjgh/pipelines/p10.py
+++ ghjghjgh/pipelines/p10.py
@@ -1,2 +1,9 @@
 with DAG():
-    pass
+    OrchestrationTarget_0 = Task(
+        task_id = "OrchestrationTarget_0", 
+        component = "OrchestrationTarget", 
+        kind = "S3Target", 
+        connector = Connection(kind = "s3"), 
+        properties = {}, 
+        format = {"properties" : {"separator" : ",", "header" : True}, "kind" : "csv", "category" : "file"}
+    )
