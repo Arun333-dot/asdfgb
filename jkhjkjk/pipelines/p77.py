0901ac0953cb123a24c678fda0767d1243939b8d
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-27 10:19:12 +0000

    dfgdfg

--- jkhjkjk/pipelines/p77.py
+++ jkhjkjk/pipelines/p77.py
@@ -40,7 +40,19 @@ with DAG():
         modelName = "model_p77_Deduplicate_1"
     )
     model_p77_Join_2 = Task(task_id = "model_p77_Join_2", component = "Model", modelName = "model_p77_Join_2")
+    model_p77_Aggregate_3 = Task(
+        task_id = "model_p77_Aggregate_3", 
+        component = "Model", 
+        modelName = "model_p77_Aggregate_3"
+    )
     model_p77_Join_3 = Task(task_id = "model_p77_Join_3", component = "Model", modelName = "model_p77_Join_3")
+    OrchestrationSource_2 = SourceTask(
+        task_id = "OrchestrationSource_2", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
     OrchestrationSource_0 = SourceTask(
         task_id = "OrchestrationSource_0", 
         component = "OrchestrationSource", 
