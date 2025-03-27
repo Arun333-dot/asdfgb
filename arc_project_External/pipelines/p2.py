5d62bb6c31245e095d570c889beda01ec4c5f1a8
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-27 05:52:28 +0000

    hjghjghjgh

--- arc_project_External/pipelines/p2.py
+++ arc_project_External/pipelines/p2.py
@@ -31,6 +31,18 @@ with DAG():
         connector = Connection(kind = "s3"), 
         format = CSVFormat(separator = ",", header = True)
     )
+    model_p2_Aggregate_2 = Task(
+        task_id = "model_p2_Aggregate_2", 
+        component = "Model", 
+        modelName = "model_p2_Aggregate_2"
+    )
+    OrchestrationSource_3 = SourceTask(
+        task_id = "OrchestrationSource_3", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
     model_p2_WindowFunction_2 = Task(
         task_id = "model_p2_WindowFunction_2", 
         component = "Model", 
