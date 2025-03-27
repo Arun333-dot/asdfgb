fd43c35cf31bb91036c2e5eec8d8fa8af1a17650
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-27 10:15:06 +0000

    fghfghfgh

--- jkhjkjk/pipelines/p77.py
+++ jkhjkjk/pipelines/p77.py
@@ -1,15 +1,22 @@
 with DAG():
+    model_p77_Deduplicate_2 = Task(
+        task_id = "model_p77_Deduplicate_2", 
+        component = "Model", 
+        modelName = "model_p77_Deduplicate_2"
+    )
     model_p77_Aggregate_1 = Task(
         task_id = "model_p77_Aggregate_1", 
         component = "Model", 
         modelName = "model_p77_Aggregate_1"
     )
+    Email_1 = Task(task_id = "Email_1", component = "Email", to = None, subject = "", body = "", includeData = False)
     model_p77_Join_1 = Task(task_id = "model_p77_Join_1", component = "Model", modelName = "model_p77_Join_1")
     model_p77_Deduplicate_1 = Task(
         task_id = "model_p77_Deduplicate_1", 
         component = "Model", 
         modelName = "model_p77_Deduplicate_1"
     )
+    model_p77_Join_2 = Task(task_id = "model_p77_Join_2", component = "Model", modelName = "model_p77_Join_2")
     OrchestrationSource_0 = SourceTask(
         task_id = "OrchestrationSource_0", 
         component = "OrchestrationSource", 
