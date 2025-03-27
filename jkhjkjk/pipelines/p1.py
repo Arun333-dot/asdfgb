1c7935764639b567298fb406087bb278cf76cb4b
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-27 11:18:45 +0000

    descripiotn

--- jkhjkjk/pipelines/p1.py
+++ jkhjkjk/pipelines/p1.py
@@ -11,8 +11,20 @@ with DAG():
         connector = {"kind" : "sftp", "type" : "connector", "properties" : {}}, 
         format = CSVFormat(separator = ",", header = True)
     )
+    model_p1_Aggregate_2 = Task(
+        task_id = "model_p1_Aggregate_2", 
+        component = "Model", 
+        modelName = "model_p1_Aggregate_2"
+    )
     model_p1_Aggregate_1 = Task(
         task_id = "model_p1_Aggregate_1", 
         component = "Model", 
         modelName = "model_p1_Aggregate_1"
+    )
+    OrchestrationSource_1 = SourceTask(
+        task_id = "OrchestrationSource_1", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
     )
--- jkhjkjk/pipelines/p17.py
+++ jkhjkjk/pipelines/p17.py
@@ -12,3 +12,10 @@ with DAG():
         modelName = "model_p17_Aggregate_1"
     )
     model_p17_Filter_1 = Task(task_id = "model_p17_Filter_1", component = "Model", modelName = "model_p17_Filter_1")
+    OrchestrationSource_1 = SourceTask(
+        task_id = "OrchestrationSource_1", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
