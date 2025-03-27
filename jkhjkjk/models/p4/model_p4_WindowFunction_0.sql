1b77a48bd3388d38e582f57a6d87270a13138bbf
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-27 10:14:46 +0000

    fghfghfghfgh

---
+++ jkhjkjk/models/p4/model_p4_WindowFunction_0.sql
@@ -0,0 +1,20 @@
+{{
+  config({    
+    "materialized": "table",
+    "alias": "prophecy__temp_p4_post_WindowFunction_0_0",
+    "database": "hive_metastore",
+    "schema": "default"
+  })
+}}
+
+WITH WindowFunction_0 AS (
+
+  SELECT * 
+  
+  FROM `` AS in0
+
+)
+
+SELECT *
+
+FROM WindowFunction_0
--- jkhjkjk/pipelines/p77.py
+++ jkhjkjk/pipelines/p77.py
@@ -1,13 +1,19 @@
 with DAG():
+    model_p77_Aggregate_1 = Task(
+        task_id = "model_p77_Aggregate_1", 
+        component = "Model", 
+        modelName = "model_p77_Aggregate_1"
+    )
+    model_p77_Join_1 = Task(task_id = "model_p77_Join_1", component = "Model", modelName = "model_p77_Join_1")
+    model_p77_Deduplicate_1 = Task(
+        task_id = "model_p77_Deduplicate_1", 
+        component = "Model", 
+        modelName = "model_p77_Deduplicate_1"
+    )
     OrchestrationSource_0 = SourceTask(
         task_id = "OrchestrationSource_0", 
         component = "OrchestrationSource", 
         kind = "S3Source", 
         connector = Connection(kind = "s3"), 
         format = CSVFormat(separator = ",", header = True)
-    )
-    model_p77_Aggregate_1 = Task(
-        task_id = "model_p77_Aggregate_1", 
-        component = "Model", 
-        modelName = "model_p77_Aggregate_1"
     )
