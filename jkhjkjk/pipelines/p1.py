a96ce5747528646fbb1aa3d04114976a684bd3f6
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-27 10:33:18 +0000

    gdfgdfg

--- jkhjkjk/pipelines/p1.py
+++ jkhjkjk/pipelines/p1.py
@@ -1,2 +1,18 @@
 with DAG():
-    pass
+    model_p1_Deduplicate_1 = Task(
+        task_id = "model_p1_Deduplicate_1", 
+        component = "Model", 
+        modelName = "model_p1_Deduplicate_1"
+    )
+    OrchestrationSource_0 = SourceTask(
+        task_id = "OrchestrationSource_0", 
+        component = "OrchestrationSource", 
+        kind = "SFTPSource", 
+        connector = {"kind" : "sftp", "type" : "connector", "properties" : {}}, 
+        format = CSVFormat(separator = ",", header = True)
+    )
+    model_p1_Aggregate_1 = Task(
+        task_id = "model_p1_Aggregate_1", 
+        component = "Model", 
+        modelName = "model_p1_Aggregate_1"
+    )
