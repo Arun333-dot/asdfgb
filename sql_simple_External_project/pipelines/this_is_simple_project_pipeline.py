c15f75cbebc4341822825998f5ea5433be5b24f1
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-28 06:16:59 +0000

    fghfghfgh

--- sql_simple_External_project/pipelines/this_is_simple_project_pipeline.py
+++ sql_simple_External_project/pipelines/this_is_simple_project_pipeline.py
@@ -1,2 +1,6 @@
 with DAG():
-    pass
+    all_type_databricks = Task(
+        task_id = "all_type_databricks", 
+        component = "Dataset", 
+        table = {"name" : "all_type_databricks", "sourceType" : "Source", "sourceName" : "qa_team.qa_database", "alias" : ""}
+    )
