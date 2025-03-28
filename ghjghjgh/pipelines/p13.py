984a629ea74a7402d53bdc88138be24fbb09385d
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-28 14:19:29 +0000

    dffdfdfdf

--- ghjghjgh/pipelines/p13.py
+++ ghjghjgh/pipelines/p13.py
@@ -1,2 +1,14 @@
 with DAG():
-    pass
+    OrchestrationTarget_0 = Task(
+        task_id = "OrchestrationTarget_0", 
+        component = "OrchestrationTarget", 
+        kind = "SharepointTarget", 
+        connector = Connection(kind = "sharepoint"), 
+        properties = {}, 
+        format = {"properties" : {"separator" : ",", "header" : True}, "kind" : "csv", "category" : "file"}
+    )
+    model_p13_WindowFunction_1 = Task(
+        task_id = "model_p13_WindowFunction_1", 
+        component = "Model", 
+        modelName = "model_p13_WindowFunction_1"
+    )
