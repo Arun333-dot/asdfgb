352776080705ede99c8427f0f62abd7bb0515fc1
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-28 06:17:50 +0000

    bhubuhu

--- sql_simple_External_project/pipelines/this_is_simple_project_pipeline.py
+++ sql_simple_External_project/pipelines/this_is_simple_project_pipeline.py
@@ -4,3 +4,15 @@ with DAG():
         component = "Dataset", 
         table = {"name" : "all_type_databricks", "sourceType" : "Source", "sourceName" : "qa_team.qa_database", "alias" : ""}
     )
+    OrchestrationSource_1 = SourceTask(
+        task_id = "OrchestrationSource_1", 
+        component = "OrchestrationSource", 
+        kind = "DatabricksVolumeSource", 
+        connector = Connection(kind = "databricks"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
+    model_this_is_simple_project_pipeline_WindowFunction_1 = Task(
+        task_id = "model_this_is_simple_project_pipeline_WindowFunction_1", 
+        component = "Model", 
+        modelName = "model_this_is_simple_project_pipeline_WindowFunction_1"
+    )
