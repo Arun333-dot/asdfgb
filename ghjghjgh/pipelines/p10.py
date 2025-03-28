a9c3b243272f0a2293964466241b735e3cac9e9e
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-03-28 15:11:09 +0000

    dsfdfsdsfddfs

--- ghjghjgh/pipelines/p10.py
+++ ghjghjgh/pipelines/p10.py
@@ -7,3 +7,16 @@ with DAG():
         properties = {}, 
         format = {"properties" : {"separator" : ",", "header" : True}, "kind" : "csv", "category" : "file"}
     )
+    empty_output_csv = SourceTask(
+        task_id = "empty_output_csv", 
+        component = "OrchestrationSource", 
+        kind = "SFTPSource", 
+        connector = Connection(kind = "sftp", id = "sftp"), 
+        format = CSVFormat(header = True, separator = ","), 
+        filePath = {
+          "type": "concat_operation", 
+          "properties": {
+            "elements": [{"type" : "literal", "properties" : {"value" : "ghjghjhgjghjghjhjghjghjghjghjghjghjhg"}}]
+          }
+        }
+    )
