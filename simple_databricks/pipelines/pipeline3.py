deafd97677b51de44b0920c9227828515bd487c2
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-04-01 09:35:38 +0000

    asdas

--- simple_databricks/pipelines/pipeline3.py
+++ simple_databricks/pipelines/pipeline3.py
@@ -1,4 +1,29 @@
 Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
 
 with DAG(Schedule = Schedule):
-    pass
+    model_pipeline3_Aggregate_1 = Task(
+        task_id = "model_pipeline3_Aggregate_1", 
+        component = "Model", 
+        modelName = "model_pipeline3_Aggregate_1"
+    )
+    model_pipeline3_Deduplicate_1 = Task(
+        task_id = "model_pipeline3_Deduplicate_1", 
+        component = "Model", 
+        modelName = "model_pipeline3_Deduplicate_1"
+    )
+    model_pipeline3_Except_1 = Task(
+        task_id = "model_pipeline3_Except_1", 
+        component = "Model", 
+        modelName = "model_pipeline3_Except_1"
+    )
+    empty_output_csv = SourceTask(
+        task_id = "empty_output_csv", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True), 
+        filePath = {
+          "type": "concat_operation", 
+          "properties": {"elements" : [{"type" : "literal", "properties" : {"value" : "randompath"}}]}
+        }
+    )
