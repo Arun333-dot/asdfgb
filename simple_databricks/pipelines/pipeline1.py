78d6a32f287957dc3caf74eab0445aa7afcba487
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-04-01 07:24:34 +0000

    dsfdsfsdfsdf

--- simple_databricks/pipelines/pipeline1.py
+++ simple_databricks/pipelines/pipeline1.py
@@ -1,10 +1,39 @@
 Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
 
 with DAG(Schedule = Schedule):
+    OrchestrationSource_1 = SourceTask(
+        task_id = "OrchestrationSource_1", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
+    OrchestrationSource_2 = SourceTask(
+        task_id = "OrchestrationSource_2", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
+    OrchestrationSource_3 = SourceTask(
+        task_id = "OrchestrationSource_3", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
     OrchestrationSource_0 = SourceTask(
         task_id = "OrchestrationSource_0", 
         component = "OrchestrationSource", 
         kind = "S3Source", 
         connector = Connection(kind = "s3"), 
         format = CSVFormat(separator = ",", header = True)
+    )
+    OrchestrationTarget_1 = Task(
+        task_id = "OrchestrationTarget_1", 
+        component = "OrchestrationTarget", 
+        kind = "S3Target", 
+        connector = Connection(kind = "s3"), 
+        properties = {}, 
+        format = {"properties" : {"separator" : ",", "header" : True}, "kind" : "csv", "category" : "file"}
     )
