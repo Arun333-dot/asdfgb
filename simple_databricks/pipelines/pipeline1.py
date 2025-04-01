cd28058755e22a2c835d1a2c8d767eed9366fd9f
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-04-01 07:22:39 +0000

    description

--- simple_databricks/pipelines/pipeline1.py
+++ simple_databricks/pipelines/pipeline1.py
@@ -1,4 +1,10 @@
 Schedule = Schedule(cron = "* 0 2 * * * *", timezone = "GMT", emails = ["email@gmail.com"], enabled = False)
 
 with DAG(Schedule = Schedule):
-    pass
+    OrchestrationSource_0 = SourceTask(
+        task_id = "OrchestrationSource_0", 
+        component = "OrchestrationSource", 
+        kind = "S3Source", 
+        connector = Connection(kind = "s3"), 
+        format = CSVFormat(separator = ",", header = True)
+    )
