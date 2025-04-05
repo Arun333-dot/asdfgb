a6f243ed786709313f4a78e0ea606ce8decb9be5
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-04-05 22:10:45 +0000

    adsads

--- hvjhjhjbbhjbhjbhjbhj/models/p1/model_p1_BulkColumnRename_1.sql
+++ hvjhjhjbbhjbhjbhjbhj/models/p1/model_p1_BulkColumnRename_1.sql
@@ -2,8 +2,8 @@
   config({    
     "materialized": "table",
     "alias": "prophecy__temp_p1_post_BulkColumnRename_1_0",
-    "database": "hive_metastore",
-    "schema": "arun123"
+    "database": "database",
+    "schema": "default"
   })
 }}
 
--- hvjhjhjbbhjbhjbhjbhj/models/p1/model_p1_Intersect_1.sql
+++ hvjhjhjbbhjbhjbhjbhj/models/p1/model_p1_Intersect_1.sql
@@ -2,8 +2,8 @@
   config({    
     "materialized": "table",
     "alias": "prophecy__temp_p1_post_Intersect_1_0",
-    "database": "hive_metastore",
-    "schema": "arun123"
+    "database": "database",
+    "schema": "default"
   })
 }}
 
