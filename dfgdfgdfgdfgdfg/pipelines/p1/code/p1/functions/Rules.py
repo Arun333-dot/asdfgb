abb1446b9dc602a9a0e67d630c975b08f6e30caa
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-05-26 20:57:26 +0000

    Add schema transformation and new rules to pipeline p1; update dataset sdfsdf structure

---
+++ dfgdfgdfgdfgdfg/pipelines/p1/code/p1/functions/Rules.py
@@ -0,0 +1,20 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from prophecy.utils import *
+
+@execute_rule
+def br(id: Column=lambda: col("ID")):
+    return when((id == lit(12)), lit(12)).otherwise(lit(- 1)).alias("output")
+
+@execute_rule
+def br2(id: Column=lambda: col("ID")):
+    return when((id == lit(12)), lit(122)).otherwise(lit(- 1)).alias("efwsdf")
+
+@execute_rule
+def br_2(id: Column=lambda: col("ID")):
+    return when((id == lit(12)), lit(12123)).otherwise(lit(- 1)).alias("asdasdas")
+
+@execute_rule
+def br5(id: Column=lambda: col("ID")):
+    return when((id == lit(12)), lit(1212)).otherwise(lit(12)).alias("asdasd")
