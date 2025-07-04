b551f8461328a9b8153f22e37a8388801cafa400
arunsharma@prophecy.io <arunsharma@prophecy.io>
2025-07-04 20:26:33 +0000

    Initialize new SQL project with model `m2`, macros, and pipeline `p2` setup

---
+++ frferfqweqweqweqw/gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'this_is_Sql_project_2',
+    version = '0.0.1',
+    packages = ['this_is_Sql_project_2'],
+    package_dir = {'this_is_Sql_project_2': '.'},
+    description = '',
+    install_requires = [],
+)
+
